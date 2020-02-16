ce268df878f07fde34b794fbd1bae48450ad3965
Jessie Tsai <jessietsai@jessies-macbook-air.local>
2020-02-16 13:00:39 -0800

    Initial Commit

---
+++ PA2 - Black Jack/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PA2 - Black Jack
+//
+//  Created by Jessie Tsai on 2/16/20.
+//  Copyright © 2020 Jessie Tsai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
