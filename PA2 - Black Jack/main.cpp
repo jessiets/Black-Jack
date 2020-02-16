ba3550ca355f0ae2009ad0d1a96edbc624edd538
Jessie Tsai <jessietsai@jessies-macbook-air.local>
2020-02-16 13:03:17 -0800

    Skeleton Code

--- PA2 - Black Jack/main.cpp
+++ PA2 - Black Jack/main.cpp
@@ -6,10 +6,132 @@
 //  Copyright © 2020 Jessie Tsai. All rights reserved.
 //
 
-#include <iostream>
+/*******************************************************************************
+ * CS 103 Twenty-One (Blackjack) PA
+ * Name: Jessie Tsai
+ * USC email: jessiets@usc.edu
+ * Comments (you want us to know):
+ *
+ *
+ ******************************************************************************/
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+// Add other #includes if you need
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+/* Prototypes */
+void shuffle(int cards[]);
+void printCard(int id);
+int cardValue(int id);
+void printHand(int hand[], int numCards);
+int getBestScore(int hand[], int numCards);
+
+const int NUM_CARDS = 52;
+
+/**
+ * Global arrays to be used as look-up tables, if desired.
+ * It is up to you if and how you want to use these
+ */
+const char suit[4] = {'H','S','D','C'};
+const char* type[13] =
+{"2","3","4","5","6","7",
+    "8","9","10","J","Q","K","A"};
+const int value[13] = {2,3,4,5,6,7,8,9,10,10,10,10,11};
+
+/**
+ * Should permute the deck of cards, effectively shuffling it.
+ * You must use the Fisher-Yates / Durstenfeld shuffle algorithm
+ *  described in the assignment writeup.
+ */
+void shuffle(int cards[])
+{
+    /******** You complete ****************/
+    
+    
+}
+
+/**
+ * Prints the card in a "pretty" format:   "type-suit"
+ *  Examples:  K-C  (King of clubs), 2-H (2 of hearts)
+ *  Valid Types are: 2,3,4,5,6,7,8,9,10,J,Q,K,A
+ *  Valid Suits are: H, D, C, S
+ */
+void printCard(int id)
+{
+    /******** You complete ****************/
+    
+    
+    
+}
+
+/**
+ * Returns the numeric value of the card.
+ *  Should return 11 for an ACE and can then
+ *  be adjusted externally based on the sum of the score.
+ */
+int cardValue(int id)
+{
+    /******** You complete ****************/
+    
+    
+}
+
+/**
+ * Should print out each card in the hand separated by a space and
+ * then print a newline at the end.
+ * Should use printCard() to print out each card.
+ */
+void printHand(int hand[], int numCards)
+{
+    /******** You complete ****************/
+    
+    
+    
+}
+
+/**
+ * Should return the total score of the provided hand.
+ * ACES should be treated as 11s to make the highest possible hand
+ *  and only being reduced to 1s as needed to avoid busting.
+ */
+int getBestScore(int hand[], int numCards)
+{
+    /******** You complete ****************/
+    
+    
+    
+    
+}
+
+/**
+ * Main program logic for the game of 21
+ */
+int main(int argc, char* argv[])
+{
+    //---------------------------------------
+    // Do not change this code -- Begin
+    //---------------------------------------
+    if(argc < 2){
+        cout << "Error - Please provide the seed value." << endl;
+        return 1;
+    }
+    int seed = atoi(argv[1]);
+    srand(seed);
+    
+    int cards[52];
+    int dhand[9];
+    int phand[9];
+    //---------------------------------------
+    // Do not change this code -- End
+    //---------------------------------------
+    
+    /******** You complete ****************/
+    
+    
+    
+    
+    
     return 0;
 }
